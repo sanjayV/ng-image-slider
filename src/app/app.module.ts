241253c8b1d6cd46e3e5816991220027ed7beea6
CodeSharkKenneth <kenneth@codeshark.co.za>
2024-05-31 01:52:46 +0200

    Angular CLI 18 upgrade

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,21 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 import { HeroService } from './hero.service';
 import {NgImageSliderModule} from '../../projects/ng-image-slider/src/lib/ng-image-slider.module';
 
-@NgModule({
-    declarations: [
+@NgModule({ declarations: [
         AppComponent
     ],
-    imports: [
-        BrowserModule,
+    bootstrap: [AppComponent], imports: [BrowserModule,
         FormsModule,
-        HttpClientModule,
-        NgImageSliderModule
-    ],
-    providers: [HeroService],
-    bootstrap: [AppComponent]
-})
+        NgImageSliderModule], providers: [HeroService, provideHttpClient(withInterceptorsFromDi())] })
 export class AppModule { }
